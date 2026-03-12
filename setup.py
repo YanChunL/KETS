94fd34bf499bfd38572fbf01af526fd70dcba52a
YanChunL <162942182+YanChunL@users.noreply.github.com>
2026-03-12 11:16:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='KETS',
+    name='KETA',
     version='1.0.0',
     author='liyanchun',
     author_email='2812028214@qq.com',
     description='A genomic scaffolding assement tools including Completeness and Accuracy',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/YanChunL/KETS.git',
+    url='https://github.com/YanChunL/KETA.git',
     packages=find_packages(),
-    scripts=['kets.py'],
+    scripts=['KETA.py'],
     python_requires='>=3.7',
     install_requires=[
         'pandas',
